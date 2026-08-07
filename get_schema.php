d978ec9b06ebbdfac79d899594d79a8915fe45bf
Ilham <dgt.ilhammln@gmail.com>
2026-08-07 22:31:40 +0700

    chore: remove scratch schema file

--- get_schema.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('includes/db_connect.php');
-$r = mysqli_query($conn, 'DESCRIBE clients');
-while($row = mysqli_fetch_assoc($r)) echo $row['Field'].' - '.$row['Type']."\n";
-echo "\n--- payments ---\n";
-$r = mysqli_query($conn, 'DESCRIBE payments');
-while($row = mysqli_fetch_assoc($r)) echo $row['Field'].' - '.$row['Type']."\n";
